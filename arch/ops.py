c9c6a3a953bce1224c909cb334d80dcd8b07a268
Aman Shrivastava <aman.srivastava999@gmail.com>
2019-08-07 17:32:08 -0400

    BUGS

--- arch/ops.py
+++ arch/ops.py
@@ -7,7 +7,7 @@ import torch
 def get_norm_layer(norm_type='instance'):
     if norm_type == 'batch':
         norm_layer = functools.partial(nn.BatchNorm2d, affine=True)
-    elif norm_layer == 'instance':
+    elif norm_type == 'instance':
         norm_layer = functools.partial(nn.InstanceNorm2d, affine=False, track_running_status=False)
     else:
         raise NotImplementedError('norm layer [%s] not found' % norm_type)
