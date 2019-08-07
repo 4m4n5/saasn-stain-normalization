240bd20612ba73eee10c357d2e9f5bdf41aa89e9
Aman Shrivastava <aman.srivastava999@gmail.com>
2019-08-07 13:02:07 -0400

    Added ops/UnetBlockGenerator

--- arch/ops.py
+++ arch/ops.py
@@ -86,3 +86,52 @@ def set_grad(nets, requires_grad=False):
             param.requires_grad = requires_grad
 
 
+class UnetSkipConnectionBlock(nn.Module):
+    def __init__(self, outer_nc, inner_nc, input_nc=None, submodule=None, outermost=False, 
+                 innermost=False, norm_layer=nn.BatchNorm2d, use_dropout=False):
+        super(UnetSkipConnectionBlock, self).__init__()
+        
+        if type(norm_layer) == functools.partial:
+            use_bias = norm_layer.func == nn.InstanceNorm2d
+        else:
+            use_bias = norm_layer == n.InstanceNorm2d
+            
+        if input_nc is None:
+            input_nc = outer_nc
+            
+        downconv = nn.Conv2d(input_nc, inner_nc, kernel_size=4, stride=2, padding=1, bias=use_bias)
+        
+        if outermost:
+            upconv = nn.ConvTranspose2d(inner_nc*2, outer_nc, kernel_size=4, stride=2, padding=1)
+            down = [downconv]
+            up = [nn.ReLU(True), upconv, nn.Tanh()]
+            model = down + [submodule] + up
+        
+        elif innermost:
+            upconv = nn.ConvTranspose2d(inner_nc, outer_nc, kernel_size=4, stride=2, padding=1, bias=use_bias)
+            down = [nn.LeakyReLU(0.2, True), downconv]
+            up = [nn.ReLU(True), upconv, norm_layer(outer_nc)]
+            model = down + up
+            
+        else:
+            upconv = nn.ConvTranspose2d(inner_nc*2, outer_nc, kernel_size=4, stride=2, padding=1, bias=use_bias)
+            down = [nn.LeakyReLU(0.2, True), downconv, norm_layer(inner_nc)]
+            up = [nn.ReLU(True), upconv, norm_layer(outer_nc)]
+            
+            if use_dropout:
+                model = down + [submodule] + up + [nn.Dropout(0.5)]
+            
+            else: 
+                model = down + [submodule] + up
+                
+            self.model = nn.Sequential(*model)
+            
+    
+    def forward(self, x):
+        if self.outermost:
+            return self.model(x)
+        
+        else:
+            return torch.cat([x, self.model(x)], 1)
+
+
