e72aa4cad9eeec27b9a974cec8130c0dc5b87a10
Aman Shrivastava <aman.srivastava999@gmail.com>
2019-08-07 13:36:04 -0400

    Added define gen function

--- arch/generators.py
+++ arch/generators.py
@@ -25,4 +25,30 @@ class UnetGenerator(nn.Module):
         return self.unet_model(input)
 
 
-def ResnetGene
+class ResnetGenerator(nn.Module):
+    def __init__(self, input_nc=3, output_nc=3, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=True, num_blocks=6):
+        super(ResnetGenerator, self).__init__()
+        if type(norm_layer) == functools.partial:
+            use_bias = norm_layer.func == nn.InstanceNorm2d
+        else:
+            use_bias = norm_layer == nn.InstanceNorm2d
+
+        res_model = [nn.ReflectionPad2d(3),
+                    conv_norm_relu(input_nc, ngf * 1, 7, norm_layer=norm_layer, bias=use_bias),
+                    conv_norm_relu(ngf * 1, ngf * 2, 3, 2, 1, norm_layer=norm_layer, bias=use_bias),
+                    conv_norm_relu(ngf * 2, ngf * 4, 3, 2, 1, norm_layer=norm_layer, bias=use_bias)]
+
+        for i in range(num_blocks):
+            res_model += [ResidualBlock(ngf * 4, norm_layer, use_dropout, use_bias)]
+
+        res_model += [dconv_norm_relu(ngf * 4, ngf * 2, 3, 2, 1, 1, norm_layer=norm_layer, bias=use_bias),
+                      dconv_norm_relu(ngf * 2, ngf * 1, 3, 2, 1, 1, norm_layer=norm_layer, bias=use_bias),
+                      nn.ReflectionPad2d(3),
+                      nn.Conv2d(ngf, output_nc, 7),
+                      nn.Tanh()]
+        self.res_model = nn.Sequential(*res_model)
+
+    def forward(self, x):
+        return self.res_model(x)
+
+
