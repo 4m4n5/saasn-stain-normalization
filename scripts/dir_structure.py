53ced752b96a6ff03bd48eaf74ce1b7e5bda0527
Aman Shrivastava <aman.srivastava999@gmail.com>
2019-08-16 16:24:07 -0400

    Changed script

--- scripts/dir_structure.py
+++ scripts/dir_structure.py
@@ -4,15 +4,22 @@ from random import randint
 import shutil
 import random
 
+# +
 # Change this to folder which contains train and valid
 src_path = '/project/DSone/as3ek/data/ganstain/500/'
 target_path = '/project/DSone/as3ek/data/ganstain/500_gstain/'
 dir_names = ['trainA', 'trainB', 'testA', 'testB']
 source_target_map = {'EE': 'A', 'Normal': 'B', 'train':'train', 'valid':'test'}
+two_domains_in_ee = True
+
 os.mkdir(target_path)
 
 
-def ee_check(file, src, folder):
+# -
+
+def ee_check(file, src, folder, two_domains_in_ee):
+    if two_domains_in_ee:
+        return True
     if src != 'EE':
         return True
     else:
@@ -29,8 +36,8 @@ for folder in os.listdir(src_path):
         if not os.path.exists(target_path + source_target_map[folder] + source_target_map[src]):
             os.mkdir(target_path + source_target_map[folder] + source_target_map[src])
         for file in os.listdir(src_path+folder + '/' + src):
-            if ee_check(file, src, folder):
-                shutil.move(src_path + folder + '/' + src + '/' + file, 
+            if ee_check(file, src, folder, two_domains_in_ee):
+                shutil.copy(src_path + folder + '/' + src + '/' + file, 
                             target_path + source_target_map[folder] + source_target_map[src] + '/' + file)
 
 len('34___2500_5750'.split('___')[0]
